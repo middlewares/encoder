4cacc63393dfd05c492cc2e6128e05700882ea34
oscarotero <oom@oscarotero.com>
2016-12-06 13:58:32 +0100

    updated tests

--- tests/EncoderTest.php
+++ tests/EncoderTest.php
@@ -5,25 +5,20 @@ namespace Middlewares\Tests;
 use Middlewares\GzipEncoder;
 use Middlewares\DeflateEncoder;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\CallableMiddleware;
-use Zend\Diactoros\ServerRequest;
-use Zend\Diactoros\Response;
+use Middlewares\Utils\Factory;
 
 class EncoderTest extends \PHPUnit_Framework_TestCase
 {
     public function testGzipEncoder()
     {
-        $request = (new ServerRequest())->withHeader('Accept-Encoding', 'gzip,deflate');
+        $request = Factory::createServerRequest()->withHeader('Accept-Encoding', 'gzip,deflate');
 
         $response = (new Dispatcher([
             new DeflateEncoder(),
             new GzipEncoder(),
-            new CallableMiddleware(function () {
-                $response = new Response();
-                $response->getBody()->write('Hello world');
-
-                return $response;
-            }),
+            function () {
+                echo 'Hello world';
+            },
         ]))->dispatch($request);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
@@ -33,17 +28,14 @@ class EncoderTest extends \PHPUnit_Framework_TestCase
 
     public function testDeflateEncoder()
     {
-        $request = (new ServerRequest())->withHeader('Accept-Encoding', 'gzip,deflate');
+        $request = Factory::createServerRequest()->withHeader('Accept-Encoding', 'gzip,deflate');
 
         $response = (new Dispatcher([
             new GzipEncoder(),
             new DeflateEncoder(),
-            new CallableMiddleware(function () {
-                $response = new Response();
-                $response->getBody()->write('Hello world');
-
-                return $response;
-            }),
+            function () {
+                echo 'Hello world';
+            },
         ]))->dispatch($request);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
@@ -53,17 +45,14 @@ class EncoderTest extends \PHPUnit_Framework_TestCase
 
     public function testNoEncoder()
     {
-        $request = (new ServerRequest())->withHeader('Accept-Encoding', 'foo');
+        $request = Factory::createServerRequest()->withHeader('Accept-Encoding', 'foo');
 
         $response = (new Dispatcher([
             new DeflateEncoder(),
             new GzipEncoder(),
-            new CallableMiddleware(function () {
-                $response = new Response();
-                $response->getBody()->write('Hello world');
-
-                return $response;
-            }),
+            function () {
+                echo 'Hello world';
+            },
         ]))->dispatch($request);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
