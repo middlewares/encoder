f50632de189ff472e6abbfb3e855b03269bcba49
oscarotero <oom@oscarotero.com>
2018-08-02 20:54:20 +0200

    updated utils to 2.1

--- src/Encoder.php
+++ src/Encoder.php
@@ -3,31 +3,19 @@ declare(strict_types = 1);
 
 namespace Middlewares;
 
+use Middlewares\Utils\Traits\HasStreamFactory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 abstract class Encoder
 {
+    use HasStreamFactory;
+
     /**
      * @var string
      */
     protected $encoding;
-
-    /**
-     * @var StreamFactoryInterface
-     */
-    private $streamFactory;
-
-    /**
-     * Set the stream factory used.
-     */
-    public function streamFactory(StreamFactoryInterface $streamFactory): self
-    {
-        $this->streamFactory = $streamFactory;
-        return $this;
-    }
 
     /**
      * Process a request and return a response.
@@ -39,8 +27,7 @@ abstract class Encoder
         if (stripos($request->getHeaderLine('Accept-Encoding'), $this->encoding) !== false
             && !$response->hasHeader('Content-Encoding')
         ) {
-            $streamFactory = $this->streamFactory ?: Utils\Factory::getStreamFactory();
-            $stream = $streamFactory->createStream($this->encode((string) $response->getBody()));
+            $stream = $this->createStream($this->encode((string) $response->getBody()));
 
             return $response
                 ->withHeader('Content-Encoding', $this->encoding)
