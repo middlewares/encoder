201a95eec03b2af1a5f27512f2a662f9bf540588
Chris Seufert <chris@modd.com.au>
2021-07-14 10:53:20 +1000

    added missing strict types & deprecated docs

--- src/DeflateEncoder.php
+++ src/DeflateEncoder.php
@@ -6,6 +6,9 @@ namespace Middlewares;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Server\MiddlewareInterface;
 
+/***
+ * @deprecated Replace with CompressEncoder
+ */
 class DeflateEncoder extends CompressEncoder implements MiddlewareInterface
 {
     public function __construct(StreamFactoryInterface $streamFactory = null)
--- src/GzipEncoder.php
+++ src/GzipEncoder.php
@@ -6,6 +6,10 @@ namespace Middlewares;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Server\MiddlewareInterface;
 
+/***
+ * @deprecated Replace with CompressEncoder
+ */
+
 class GzipEncoder extends CompressEncoder implements MiddlewareInterface
 {
     public function __construct(StreamFactoryInterface $streamFactory = null)
